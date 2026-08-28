c56a45b8cab4e3addcdb4fbbe0cfe95dcc614d5f
Joolpod <willyju1178@gmail.com>
2026-08-28 13:31:30 -0400

    Basic ALU implementation

---
+++ rtl/alu.sv
@@ -0,0 +1,20 @@
+module alu(
+    input logic [31:0] a,
+    input logic [31:0] b,
+    input logic [3:0] op,
+    output logic [31:0] out
+);
+
+    always_comb begin
+        case (op)
+            4'b0000: out = a + b;
+            4'b0001: out = a * b;
+            4'b0010: out = a & b;
+            4'b0011: out = a | b;
+            4'b0100: out = ~a;
+            default: out = 32'b0;
+        endcase
+    end
+
+endmodule
+
