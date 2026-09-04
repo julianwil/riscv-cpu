139c8cf6b02bc9895806ab5bb083ae9ec9c7b8c3
Joolpod <willyju1178@gmail.com>
2026-09-04 15:34:56 -0400

    Added all remaining ALU operations

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -12,6 +12,21 @@ module alu(
             4'b0010: out = a & b;
             4'b0011: out = a | b;
             4'b0100: out = a ^ b;
+            4'b0101: out = a << b;
+            4'b0110: out = a >> b;
+            4'b0111: out = $signed(a) >>> b;
+            4'b1000: begin
+                if (a < b) 
+                    out = 32'b1;
+                else 
+                    out = 32'b0;
+            end
+            4'b1001: begin
+                if ($signed(a) < $signed(b)) 
+                    out = 32'b1;
+                else 
+                    out = 32'b0;
+            end
             default: out = 32'b0;
         endcase
     end
