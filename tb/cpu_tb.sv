901920e5503aacef6303333dd0e6fbecbded7683
Joolpod <willyju1178@gmail.com>
2026-09-04 14:41:03 -0400

    Added dump_state task to testbench, adjusted dispalyed values

--- tb/cpu_tb.sv
+++ tb/cpu_tb.sv
@@ -3,6 +3,17 @@ module tb;
     logic clk;
     logic reset;
 
+    task automatic dump_state();
+        $display(                                                                                                                                                    
+            "PC=%h INSTR=%h x1=%h x2=%h x3=%h",                                                                                                                            
+            dut.pc_val,                                                                                                                                              
+            dut.im0.instr,                                                                                                                                           
+            dut.rf0.registers[1],                                                                                                                                    
+            dut.rf0.registers[2],
+            dut.rf0.registers[3]
+        );                 
+    endtask
+
     cpu dut(
         .clk(clk),
         .reset(reset)
@@ -14,27 +25,15 @@ module tb;
         clk = 0;
         reset = 1;
         
-        dut.im0.imem[0] = 32'h02A00293;
-
         #20;
         reset = 0;
 
-        $display(
-            "PC=%h INSTR=%h x5=%h",
-            dut.pc_val,
-            dut.im0.instr,
-            dut.rf0.registers[5]
-        );
-
+        dump_state();
+        
         repeat (10) begin
             @(posedge clk);
             #1;
-            $display(
-                "PC=%h INSTR=%h x5=%h",
-                dut.pc_val,
-                dut.im0.instr,
-                dut.rf0.registers[5]
-            );
+            dump_state();
         end
 
         $finish;
